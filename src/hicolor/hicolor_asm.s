88ff4f4060079792ec89f95b7b11315ece66dd0e
DavidJCobb <831497+DavidJCobb@users.noreply.github.com>
2025-09-18 05:03:04 -0400

    Update hicolor_asm.s
    Reduced the number of registers we have to push and pop per CpuFastSet call by 1. Still seeing artifacts on-screen when PALETTES_EARLY_BY_SCANLINES < 2, though...

--- src/hicolor/hicolor_asm.s
+++ src/hicolor/hicolor_asm.s
@@ -6,41 +6,60 @@
 @ 16 colors * 2 bytes per color = 32 bytes == 1 << 5
 .set BITSHIFT_PER_PALETTE, 5
 
-@ r0: source
-@ r1: destination
-@ r2: size in dwords
+@ Syscall. Copies memory in 32-byte blocks.
+@    r0: source
+@    r1: destination
+@    r2: size in dwords
+@ Increments r1 after each 32-byte copy.
+@ Registers  r2 through r9 will be clobbered.
 .macro cpufastset
    swi #0x0C
 .endm
+@
+@ As an aside, CpuSet increments r3 and clobbers r0 and r5.
 
-@ r4: argument: scanline base     pointer
-@ r5: argument: palettes blending pointer
+@
+@ Registers used:
+@
+@    r0:  Current scanline VRAM-to-HiColor index.
+@    r1:  Destination pointer (VRAM palette memory).
+@    r2:  Constant argument to CpuFastSet syscall.
+@    r4:  Scanline base pointer. Transferred in from arg1 (r0).
+@    r10: Blending palettes pointer. Transferred in from arg2 (r1).
+@
+@ Only registers above r9 can avoid being clobbered by CpuFastSet, 
+@ but registers above r7 can only be used in specific opcodes. If 
+@ we use these registers whenever possible, we reduce the amount 
+@ of stuff we have to push and pop around each CpuFastSet call.
+@
 
 .macro loop_body index_a:req
    ldrb r0, [r4, #\index_a]
    cmp  r0, #255
    beq  HiColor_HBlank_Asm_lAfterCopy\index_a
    lsl  r0, r0, #BITSHIFT_PER_PALETTE
-   add  r0, r5
-   push {r1,r2,r4,r5}
+   add  r0, r10
+   push {r1,r2,r4}
    cpufastset
-   pop  {r1,r2,r4,r5}
+   pop  {r1,r2,r4}
 HiColor_HBlank_Asm_lAfterCopy\index_a :
    add  r1, #PLTT_SIZE_4BPP
 .endm
 
-@thumb_func_start HiColor_HBlank_Asm
 .align 2, 0
 .global HiColor_HBlank_Asm
 .thumb
 .thumb_func
 .type HiColor_HBlank_Asm, %function
 HiColor_HBlank_Asm:
-   push {r2,r4,r5,lr}
-   mov  r4, r0
-   mov  r5, r1
-   ldr  r1, =OBJ_PLTT
-   mov  r2, #(PLTT_SIZE_4BPP / 4)
+   push {r2,r4,lr}
+   mov  r4, r10
+   push {r4}
+   @
+   mov  r4,  r0
+   mov  r10, r1
+   ldr  r1,  =OBJ_PLTT
+   mov  r2,  #(PLTT_SIZE_4BPP / 4)
    loop_body 0
    loop_body 1
    loop_body 2
@@ -57,6 +76,8 @@ HiColor_HBlank_Asm:
    loop_body 13
    loop_body 14
    loop_body 15
-   pop {r2,r4,r5,pc}
-@thumb_func_end HiColor_HBlank_Asm
+   @
+   pop  {r4}
+   mov  r10, r4
+   pop  {r2,r4,pc}
 .size HiColor_HBlank_Asm, .-HiColor_HBlank_Asm
